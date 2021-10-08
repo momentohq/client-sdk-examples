d84f40b2bf8c6b13da70fc2198584692fd27af3d
gautamomento <gautam@momentohq.com>
2021-10-08 15:23:35 -0700

    feat: Prepare examples for customer onboarding

--- java-gradle/lib/build.gradle.kts
+++ java-gradle/lib/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-    implementation("momento.sandbox:momento-sdk:0.8.0")
+    implementation("momento.sandbox:momento-sdk:0.10.0")
 
     // Use JUnit Jupiter for testing.
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
--- java-gradle/lib/src/main/java/momento/client/example/MomentoCacheApplication.java
+++ java-gradle/lib/src/main/java/momento/client/example/MomentoCacheApplication.java
@@ -8,52 +8,52 @@ import momento.sdk.messages.MomentoCacheResult;
 
 public class MomentoCacheApplication {
 
-  private static final String MOMENTO_AUTH_TOKEN = "<TEST_AUTH_TOKEN>";
-  private static final String MOMENTO_ENDPOINT =
-      null; // Fill this in, only if provided by the Momento Team
-  private static final String CACHE_NAME = "dummy";
+  private static final String MOMENTO_AUTH_TOKEN = "<YOUR TEST AUTH TOKEN>";
+  private static final String CACHE_NAME = "myCache";
 
   public static void main(String[] args) throws InterruptedException {
     run();
   }
 
   private static void run() throws InterruptedException {
-    Momento momento =
-        Momento.builder().authToken(MOMENTO_AUTH_TOKEN).endpointOverride(MOMENTO_ENDPOINT).build();
+    Momento momento = Momento.builder(MOMENTO_AUTH_TOKEN).build();
 
     // Create or Get Cache
-    Cache cache = null;
     try {
-      cache = momento.createCache(CACHE_NAME);
-      System.out.println("Created a new Cache with name: " + CACHE_NAME);
+      momento.createCache(CACHE_NAME);
     } catch (CacheAlreadyExistsException e) {
       // Cache already exists, so use it.
       System.out.println("Found a cache with name: " + CACHE_NAME);
-      cache = momento.getCache(CACHE_NAME);
     }
+    Cache cache = momento.getCache(CACHE_NAME);
 
     // Get what you set
     String getWhatYouSetKey = "MyFirstKey";
     String getWhatYouSetValue = "CacheMePlease";
     int ttl = 10; // second
     cache.set(getWhatYouSetKey, getWhatYouSetValue, ttl);
+
     // Read the value and it must be a HIT!
     CacheGetResponse getWhatYouSet = cache.get(getWhatYouSetKey);
-    assert getWhatYouSet.getResult() == MomentoCacheResult.Hit : "Expect Cache Hit, But was a Miss";
+    assert getWhatYouSet.result() == MomentoCacheResult.Hit : "Expect Cache Hit, But was a Miss";
     System.out.println("Cache Hit!!");
-    assert getWhatYouSet.asStringUtf8().equals(getWhatYouSetValue)
+    assert getWhatYouSet.asStringUtf8().get().equals(getWhatYouSetValue)
         : String.format(
             "Expected value `%s` but found `%s`", getWhatYouSetValue, getWhatYouSet.asStringUtf8());
     System.out.println(
         String.format(
             "Expected value `%s` ==  Looked up value `%s`",
-            getWhatYouSetValue, getWhatYouSet.asStringUtf8()));
+            getWhatYouSetValue, getWhatYouSet.asStringUtf8().get()));
 
     // Test TTL Enforcement
+    System.out.println("");
+    System.out.println("Checking TTL Enforcement");
     Thread.sleep(ttl * 1000); // millisecond
+    // Read the value and it must be a MISS!
     CacheGetResponse ttlResponse = cache.get(getWhatYouSetKey);
-    assert ttlResponse.getResult() == MomentoCacheResult.Miss
-        : "Item found, but it is past the TTL";
+    assert ttlResponse.result() == MomentoCacheResult.Miss : "Item found, but it is past the TTL";
+    assert ttlResponse.asStringUtf8().isPresent() == false
+        : "Item value found, but it shouldn't be present";
     System.out.println("Item dropped from Cache after expiry!");
   }
 }
