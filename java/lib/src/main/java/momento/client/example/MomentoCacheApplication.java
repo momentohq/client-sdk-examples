9004d7620b88e297d9e1d0dd3204c7d40a2b4c9e
gautamomento <gautam@momentohq.com>
2021-10-12 13:49:23 -0700

    test

--- java/lib/src/main/java/momento/client/example/MomentoCacheApplication.java
+++ java/lib/src/main/java/momento/client/example/MomentoCacheApplication.java
@@ -6,21 +6,20 @@ import momento.sdk.Momento;
 public class MomentoCacheApplication {
 
   private static final String MOMENTO_AUTH_TOKEN = System.getenv("MOMENTO_AUTH_TOKEN");
+  private static final String CACHE_NAME = "cache";
+  private static final String KEY = "key";
+  private static final String VALUE = "value";
+  private static final int ITEM_TTL_SECONDS = 10;
 
   public static void main(String[] args) {
-    String cacheName = "cache";
-    String key = "key";
-    String value = "value";
-    int itemTtlSeconds = 10;
     System.out.println("Running Momento Cache Application");
 
     try(Momento momento = Momento.builder(MOMENTO_AUTH_TOKEN).build()) {
-      try(Cache cache = momento.createOrGetCache(cacheName)) {
-        cache.set(key, value, itemTtlSeconds); // key -> value with 10 second TTL
-        String resp = cache.get(key).asStringUtf8().get();
+      try(Cache cache = momento.createOrGetCache(CACHE_NAME)) {
+        cache.set(KEY, VALUE, ITEM_TTL_SECONDS); // key -> value with 10 second TTL
+        String resp = cache.get(KEY).asStringUtf8().get();
         assert resp.equals(resp);
       }
     }
-
   }
 }
