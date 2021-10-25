724159c9e41bd763de355e74173fcdd64081a96d
Matt Straathof <matt.straat@gmail.com>
2021-10-25 12:54:49 -0700

    fix: bump sdk version to 0.2.2

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,9 +1,9 @@
-import {Momento} from '@momento/sdk';
+import {Momento, MomentoCacheResult} from '@momento/sdk';
 
-const cacheName = "cache2"
+const cacheName = "cache"
 const cacheKey = "key"
 const cacheValue = "value"
-const ttl = 5000
+const ttl = 100
 const authToken = process.env.MOMENTO_AUTH_TOKEN
 if (!authToken) {
     throw new Error("Missing required environment variable MOMENTO_AUTH_TOKEN")
@@ -13,12 +13,17 @@ const momento = new Momento(authToken);
 
 const main = async () => {
     const cache = await momento.createOrGetCache(cacheName, {
-        defaultTtlSeconds: 100
+        defaultTtlSeconds: ttl
     });
     console.log(`Storing key=${cacheKey}, value=${cacheValue}, ttl=${ttl}`)
-    await cache.set(cacheKey, cacheValue, ttl)
+
+    await cache.set(cacheKey, cacheValue)
     const getResp = await cache.get(cacheKey)
-    console.log(`result: ${getResp.text()}`)
+    if (getResp.result === MomentoCacheResult.Hit) {
+        console.log(`cache hit: ${getResp.text()}`)
+    } else {
+        console.log('cache miss')
+    }
 }
 
 main().then(() => {
