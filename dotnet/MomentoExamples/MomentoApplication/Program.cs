2bfd3551eed645a5a2343f555c2ba44b05463217
gautamomento <gautam@momentohq.com>
2021-10-28 11:20:01 -0700

    feat: Add dotnet example

---
+++ dotnet/MomentoExamples/MomentoApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MomentoSdk;
+using MomentoSdk.Responses;
+
+namespace MomentoApplication
+{
+    class Program
+    {
+        static readonly String MOMENTO_AUTH_TOKEN = Environment.GetEnvironmentVariable("MOMENTO_AUTH_TOKEN");
+        static readonly String CACHE_NAME = "cache";
+        static readonly String KEY = "key";
+        static readonly String VALUE = "value";
+        
+        static void Main(string[] args)
+
+        {
+            Momento momento = new Momento(MOMENTO_AUTH_TOKEN);
+
+            MomentoCache cache = momento.CreateOrGetCache(CACHE_NAME, 60);
+
+            CacheSetResponse response = cache.Set(KEY, VALUE, 60);
+            CacheGetResponse getResponse = cache.Get(KEY);
+
+            Console.WriteLine(getResponse.result);
+            Console.WriteLine(getResponse.Text());
+
+            getResponse = cache.Get("World");
+            Console.WriteLine(getResponse.result);
+            Console.WriteLine(getResponse.Text());
+    
+        }
+    }
+}
