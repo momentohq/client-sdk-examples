4a7cdbc47625a4e557736a8cd307b137d7f164b1
gautamomento <gautam@momentohq.com>
2021-10-29 10:37:12 -0700

    update example

--- dotnet/MomentoExamples/MomentoApplication/Program.cs
+++ dotnet/MomentoExamples/MomentoApplication/Program.cs
@@ -8,26 +8,19 @@ namespace MomentoApplication
     {
         static readonly String MOMENTO_AUTH_TOKEN = Environment.GetEnvironmentVariable("MOMENTO_AUTH_TOKEN");
         static readonly String CACHE_NAME = "cache";
-        static readonly String KEY = "key";
-        static readonly String VALUE = "value";
+        static readonly String KEY = "MyKey";
+        static readonly String VALUE = "MyData";
         
         static void Main(string[] args)
-
         {
-            Momento momento = new Momento(MOMENTO_AUTH_TOKEN);
-
-            MomentoCache cache = momento.CreateOrGetCache(CACHE_NAME, 60);
-
-            CacheSetResponse response = cache.Set(KEY, VALUE, 60);
+            using Momento momento = new Momento(MOMENTO_AUTH_TOKEN);
+            using MomentoCache cache = momento.CreateOrGetCache(CACHE_NAME, 60);
+            Console.WriteLine($"Setting Key: {KEY} with Value: {VALUE}");
+            cache.Set(KEY, VALUE);
+            Console.WriteLine($"Get Value for  Key: {KEY}");
             CacheGetResponse getResponse = cache.Get(KEY);
-
-            Console.WriteLine(getResponse.result);
-            Console.WriteLine(getResponse.String());
-
-            getResponse = cache.Get("World");
-            Console.WriteLine(getResponse.result);
-            Console.WriteLine(getResponse.String());
-    
+            Console.WriteLine($"Lookup Result: {getResponse.result}");
+            Console.WriteLine($"Lookedup Value: {getResponse.String()}, Stored Value: {VALUE}");
         }
     }
 }
