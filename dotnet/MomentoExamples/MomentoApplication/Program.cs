dc12441710e21808d1e5cb2e861e3a276a92098c
gautamomento <89037104+gautamomento@users.noreply.github.com>
2021-10-29 10:49:21 -0700

    feat: Add dotnet example (#19)
    * feat: Add dotnet example
    
    * fix: bump sdk version to 0.3.6
    
    * build: add ci for dotnet
    
    * docs: update readme to include dotnet
    
    * docs: add readme for csharp
    
    * update example
    
    * Update README
    
    * Link to sample code
    
    Co-authored-by: Matt Straathof <matt.straat@gmail.com>

---
+++ dotnet/MomentoExamples/MomentoApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MomentoSdk;
+using MomentoSdk.Responses;
+
+namespace MomentoApplication
+{
+    class Program
+    {
+        static readonly String MOMENTO_AUTH_TOKEN = Environment.GetEnvironmentVariable("MOMENTO_AUTH_TOKEN");
+        static readonly String CACHE_NAME = "cache";
+        static readonly String KEY = "MyKey";
+        static readonly String VALUE = "MyData";
+        
+        static void Main(string[] args)
+        {
+            using Momento momento = new Momento(MOMENTO_AUTH_TOKEN);
+            using MomentoCache cache = momento.CreateOrGetCache(CACHE_NAME, 60);
+            Console.WriteLine($"Setting Key: {KEY} with Value: {VALUE}");
+            cache.Set(KEY, VALUE);
+            Console.WriteLine($"Get Value for  Key: {KEY}");
+            CacheGetResponse getResponse = cache.Get(KEY);
+            Console.WriteLine($"Lookup Result: {getResponse.result}");
+            Console.WriteLine($"Lookedup Value: {getResponse.String()}, Stored Value: {VALUE}");
+        }
+    }
+}
