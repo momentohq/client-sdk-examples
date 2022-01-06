c7c3435c3b3de3b2299bcfb2fe46d4683ebe0ef2
gautamomento <89037104+gautamomento@users.noreply.github.com>
2022-01-05 20:43:30 -0800

    feat: Add debug options to example (#38)
    * feat: Add debug options to example
    
    * revert cache name
    
    * tweak README (#39)
    
    * tweak README
    
    * fix
    
    Co-authored-by: danielamiao <2438477+danielamiao@users.noreply.github.com>

--- python/example.py
+++ python/example.py
@@ -1,10 +1,26 @@
 import os
 import momento.momento as momento
+import logging
+
 
 _MOMENTO_AUTH_TOKEN = os.getenv('MOMENTO_AUTH_TOKEN')
 _CACHE_NAME = 'cache'
 _KEY = 'MyKey'
 _VALUE = 'MyValue'
+_DEBUG_MODE = os.getenv('DEBUG')
+
+
+if (_DEBUG_MODE == 'true'):
+    logger = logging.getLogger('momentosdk')
+    logger.setLevel(logging.DEBUG)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.DEBUG)
+    logger.addHandler(consoleHandler)
+
+    formatter = logging.Formatter('%(asctime)s  %(name)s  %(levelname)s: %(message)s')
+    consoleHandler.setFormatter(formatter)
+
 
 def _print_start_banner():
     print('******************************************************************')
