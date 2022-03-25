93c3a05dc9f9d59ebe183c53a2fd419277dea36b
Erika Tharp <eem@momentohq.com>
2022-03-25 16:30:40 -0700

    update EN/JP README; use @gomomento/sdk (#79)

--- javascript/index.ts
+++ javascript/index.ts
@@ -1,41 +1,47 @@
-import {SimpleCacheClient, CacheGetStatus, AlreadyExistsError} from '@momento/sdk';
+import {
+  SimpleCacheClient,
+  CacheGetStatus,
+  AlreadyExistsError,
+} from "@gomomento/sdk";
 
-const cacheName = "cache"
-const cacheKey = "key"
-const cacheValue = "value"
-const ttl = 60
-const authToken = process.env.MOMENTO_AUTH_TOKEN
+const cacheName = "cache";
+const cacheKey = "key";
+const cacheValue = "value";
+const ttl = 60;
+const authToken = process.env.MOMENTO_AUTH_TOKEN;
 if (!authToken) {
-    throw new Error("Missing required environment variable MOMENTO_AUTH_TOKEN")
+  throw new Error("Missing required environment variable MOMENTO_AUTH_TOKEN");
 }
 
 const defaultTtl = 60;
 const momento = new SimpleCacheClient(authToken, defaultTtl);
 
 const main = async () => {
-    try {
-        await momento.createCache(cacheName);
-    } catch(e) {
-        if (e instanceof AlreadyExistsError) {
-            console.log("cache already exists")
-        } else {
-            throw e
-        }
-    }
-
-    console.log(`Storing key=${cacheKey}, value=${cacheValue}, ttl=${ttl}`)
-    await momento.set(cacheName, cacheKey, cacheValue)
-    const getResp = await momento.get(cacheName, cacheKey)
-
-    if (getResp.status === CacheGetStatus.Hit) {
-        console.log(`cache hit: ${getResp.text()}`)
+  try {
+    await momento.createCache(cacheName);
+  } catch (e) {
+    if (e instanceof AlreadyExistsError) {
+      console.log("cache already exists");
     } else {
-        console.log('cache miss')
+      throw e;
     }
-}
+  }
 
-main().then(() => {
-    console.log("success!!")
-}).catch((e) => {
-    console.error("failed to get from cache", e)
-})
+  console.log(`Storing key=${cacheKey}, value=${cacheValue}, ttl=${ttl}`);
+  await momento.set(cacheName, cacheKey, cacheValue);
+  const getResp = await momento.get(cacheName, cacheKey);
+
+  if (getResp.status === CacheGetStatus.Hit) {
+    console.log(`cache hit: ${getResp.text()}`);
+  } else {
+    console.log("cache miss");
+  }
+};
+
+main()
+  .then(() => {
+    console.log("success!!");
+  })
+  .catch((e) => {
+    console.error("failed to get from cache", e);
+  });
