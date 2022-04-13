436d30d897b0826eb67f69e588de3b0d39d86f78
Wanda Chen <wanda@momentohq.com>
2022-04-12 17:52:03 -0700

    minor code cleanup

--- dotnet/MomentoExamples/MomentoApplicationPresignedUrl/Program.cs
+++ dotnet/MomentoExamples/MomentoApplicationPresignedUrl/Program.cs
@@ -13,8 +13,8 @@ namespace MomentoApplicationPresignedUrl
         private static readonly string ENDPOINT = Environment.GetEnvironmentVariable("ENDPOINT");
         private static readonly uint URL_TTL_MINUTES = 10;
         private static readonly string CACHE_NAME = "cache";
-        private static readonly string KEY = "MyKey";
-        private static readonly string VALUE = "MyData";
+        private static readonly string OBJECT_KEY = "MyKey";
+        private static readonly string OBJECT_VALUE = "MyData";
         private static readonly uint OBJECT_TTL_SECONDS = 60 * 60 * 24 * 14; // 14 days
 
         private static readonly HttpClient client = new HttpClient();
@@ -24,46 +24,39 @@ namespace MomentoApplicationPresignedUrl
             return System.Convert.ToBase64String(plainTextBytes);
         }
 
-        private static async Task RunPresignedUrlExample(string setUrl, string getUrl)
+        private static async Task RunPresignedUrlExample(Uri setUri, Uri getUri)
         {
-            Console.WriteLine($"Posting value with signed URL for {CacheOperation.SET}: {VALUE}");
-            var json = JsonConvert.SerializeObject(Base64Encode(VALUE));
+            Console.WriteLine($"Posting value with signed URL for {CacheOperation.SET}: {OBJECT_VALUE}");
+            var json = JsonConvert.SerializeObject(Base64Encode(OBJECT_VALUE));
             var data = new StringContent(json, Encoding.UTF8, "application/json");
-            var setResponse = await client.PostAsync(setUrl, data);
+            var setResponse = await client.PostAsync(setUri, data);
             setResponse.EnsureSuccessStatusCode();
             
-            var getResponse = await client.GetStringAsync(getUrl);
-            Console.WriteLine($"Lookedup value with signed URL for {CacheOperation.GET}: {getResponse}");
+            var getResponse = await client.GetStringAsync(getUri);
+            Console.WriteLine($"Retrieved value with signed URL for {CacheOperation.GET}: {getResponse}");
         }
 
         static async Task Main(string[] args)
         {
             // prepare requests
             uint expiryEpochSeconds = (uint) DateTimeOffset.UtcNow.AddMinutes(URL_TTL_MINUTES).ToUnixTimeSeconds();
-            var setReq = new SigningRequest(CACHE_NAME, KEY, CacheOperation.SET, expiryEpochSeconds)
-            {
-                TtlSeconds = OBJECT_TTL_SECONDS
-            };
-            var getReq = new SigningRequest(CACHE_NAME, KEY, CacheOperation.GET, expiryEpochSeconds);
-            Console.WriteLine($"Request claims: cache = {CACHE_NAME}, key = {KEY}, exp = {expiryEpochSeconds}, ttl (for set) = {OBJECT_TTL_SECONDS}");
+            var setReq = new SigningRequest(CACHE_NAME, OBJECT_KEY, CacheOperation.SET, expiryEpochSeconds) { TtlSeconds = OBJECT_TTL_SECONDS };
+            var getReq = new SigningRequest(CACHE_NAME, OBJECT_KEY, CacheOperation.GET, expiryEpochSeconds);
+            Console.WriteLine($"Request claims: cache = {CACHE_NAME}, key = {OBJECT_KEY}, exp = {expiryEpochSeconds}, ttl (for set) = {OBJECT_TTL_SECONDS}");
 
             // create presigned urls
             MomentoSigner signer = new MomentoSigner(SIGNING_KEY);
             var setUrl = signer.CreatePresignedUrl(ENDPOINT, setReq);
             var getUrl = signer.CreatePresignedUrl(ENDPOINT, getReq);
 
-            Uri uriResult;
-            bool result = Uri.TryCreate(setUrl, UriKind.Absolute, out uriResult);
-            if (result) {
-                Console.WriteLine($"Pre-signed URL for {CacheOperation.SET}:\n{setUrl}");
-            }
-            result = Uri.TryCreate(getUrl, UriKind.Absolute, out uriResult);
-            if (result) {
-                Console.WriteLine($"Pre-signed URL for {CacheOperation.GET}:\n{getUrl}");
-            }
+            Uri setUri, getUri;
+            Uri.TryCreate(setUrl, UriKind.Absolute, out setUri);
+            Console.WriteLine($"Pre-signed URL for {CacheOperation.SET}:\n{setUrl}");
+            Uri.TryCreate(getUrl, UriKind.Absolute, out getUri);
+            Console.WriteLine($"Pre-signed URL for {CacheOperation.GET}:\n{getUrl}");
 
             // set and get using presigned urls
-            await RunPresignedUrlExample(setUrl, getUrl);
+            await RunPresignedUrlExample(setUri, getUri);
         }
     }
 }
