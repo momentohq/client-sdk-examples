69d08436352674f2a05a38f6135547bfdf8cb9ca
Chris Price <chris@momentohq.com>
2022-07-07 10:01:32 -0700

    chore: add linting config for javascript example
    This commit applies our typical eslint/prettier settings to
    the javascript example project.  No functional changes.

--- javascript/index.ts
+++ javascript/index.ts
@@ -1,20 +1,26 @@
-import {AlreadyExistsError, CacheGetStatus, LogLevel, LogFormat, SimpleCacheClient,} from '@gomomento/sdk';
+import {
+  AlreadyExistsError,
+  CacheGetStatus,
+  LogLevel,
+  LogFormat,
+  SimpleCacheClient,
+} from '@gomomento/sdk';
 
-const cacheName = "cache";
-const cacheKey = "key";
-const cacheValue = "value";
+const cacheName = 'cache';
+const cacheKey = 'key';
+const cacheValue = 'value';
 const ttl = 60;
 const authToken = process.env.MOMENTO_AUTH_TOKEN;
 if (!authToken) {
-  throw new Error("Missing required environment variable MOMENTO_AUTH_TOKEN");
+  throw new Error('Missing required environment variable MOMENTO_AUTH_TOKEN');
 }
 
 const defaultTtl = 60;
 const momento = new SimpleCacheClient(authToken, defaultTtl, {
   loggerOptions: {
     level: LogLevel.DEBUG,
-    format: LogFormat.CONSOLE
-  }
+    format: LogFormat.CONSOLE,
+  },
 });
 
 const main = async () => {
@@ -22,37 +28,37 @@ const main = async () => {
     await momento.createCache(cacheName);
   } catch (e) {
     if (e instanceof AlreadyExistsError) {
-      console.log("cache already exists");
+      console.log('cache already exists');
     } else {
       throw e;
     }
   }
 
-  console.log("Listing caches:");
+  console.log('Listing caches:');
   let token;
   do {
     const listResp = await momento.listCaches();
-    listResp.getCaches().forEach((cacheInfo) => {
+    listResp.getCaches().forEach(cacheInfo => {
       console.log(`${cacheInfo.getName()}`);
     });
     token = listResp.getNextToken();
-  } while (token != null);
+  } while (token !== null);
 
   console.log(`Storing key=${cacheKey}, value=${cacheValue}, ttl=${ttl}`);
   await momento.set(cacheName, cacheKey, cacheValue);
   const getResp = await momento.get(cacheName, cacheKey);
 
   if (getResp.status === CacheGetStatus.Hit) {
-    console.log(`cache hit: ${getResp.text()}`);
+    console.log(`cache hit: ${String(getResp.text())}`);
   } else {
-    console.log("cache miss");
+    console.log('cache miss');
   }
 };
 
 main()
   .then(() => {
-    console.log("success!!");
+    console.log('success!!');
   })
-  .catch((e) => {
-    console.error("failed to get from cache", e);
+  .catch(e => {
+    console.error('failed to get from cache', e);
   });
