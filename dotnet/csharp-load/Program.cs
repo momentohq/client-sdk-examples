0bdca2ecf6aee211bc80e7986087bfcd2750e0e7
Kenny <kenny@momentohq.com>
2022-08-05 18:33:43 -0700

    feat: a more intricate C# Momento.Protos example
    This is a load generator in C# which does not use the Momento client.
    Instead, it uses the generated grpc and protocol buffers structures
    from [the client_protos repository](https://github.com/momentohq/client_protos).
    
    Generally folks should use our client unless there's a reason to
    use the protos directly - but anyone who wants to do a little more
    heavy lifting themselves is more than welcome to give it a go!

---
+++ dotnet/csharp-load/Program.cs
@@ -0,0 +1,88 @@
+﻿
+namespace application;
+
+class Program {
+    static async Task Main(string[] args)
+    {
+        string endpoint;
+        string authToken;
+        string cache;
+        uint hz;
+        uint reportInterval;
+        uint clients;
+        uint concurrency;
+        try
+        {
+            endpoint = args.Where((arg) => arg.StartsWith("--endpoint="))
+                    .First()
+                    .Split("=")[1];
+
+            authToken = args.Where((arg) => arg.StartsWith("--token="))
+                    .First()
+                    .Split("=")[1];
+
+            cache = args.Where((arg) => arg.StartsWith("--cache="))
+                    .First()
+                    .Split("=")[1];
+
+            hz = uint.Parse(
+                args.Where((arg) => arg.StartsWith("--hz="))
+                    .FirstOrDefault("=100")
+                    .Split("=")[1]
+            );
+
+            reportInterval = uint.Parse(
+                args.Where((arg) => arg.StartsWith("--interval="))
+                    .FirstOrDefault("=10")
+                    .Split("=")[1]
+            );
+
+            clients = uint.Parse(
+                args.Where((arg) => arg.StartsWith("--clients="))
+                    .FirstOrDefault("=" + Math.Max(1, Environment.ProcessorCount - 1))
+                    .Split("=")[1]
+            );
+
+            // Requests per client. Momento defaults to 100 maximum per client instance.
+            concurrency = uint.Parse(
+                args.Where((arg) => arg.StartsWith("--concurrency="))
+                    .FirstOrDefault("=100")
+                    .Split("=")[1]
+            );
+        }
+        catch (Exception)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Momento C# Load Generator\n");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("  --endpoint=cache.cell-us-east-1-1.prod.a.momentohq.com  [required]");
+            Console.WriteLine("  --token=your_momento_token                              [required]");
+            Console.WriteLine("  --cache=cache_to_test                                   [required]");
+            Console.WriteLine("  --hz=2000                                               [100]");
+            Console.WriteLine("    Set the request rate to attempt");
+            Console.WriteLine("  --clients=5                                             [processorcount - 1]");
+            Console.WriteLine("    Set the number of concurrent clients to use to attempt the configured hz");
+            Console.WriteLine("  --concurrency=5                                         [100]");
+            Console.WriteLine("    Set the maximum number of concurrent h2 streams per client");
+            Console.WriteLine("  --interval=10                                           [10]");
+            Console.WriteLine("    Set the console report interval (in seconds)");
+            Environment.Exit(1);
+            throw new Exception("impossible");
+        }
+
+        Console.WriteLine("Starting {0:D}hz", hz);
+
+        var cancellationSource = new CancellationTokenSource();
+
+        Console.CancelKeyPress += delegate {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Error.WriteLine("\nTerminating load threads...");
+            Console.ResetColor();
+            cancellationSource.Cancel();
+        };
+
+        await new LoadGenerator(cancellationSource, endpoint, authToken, cache).Run(hz, reportInterval, clients, concurrency);
+
+        Console.WriteLine("All done");
+    }
+}
---
+++ dotnet/csharp-load/load/Printing.cs
@@ -0,0 +1,62 @@
+﻿using HdrHistogram;
+
+namespace Printing;
+
+public static class Printing
+{
+    public record class Column(
+        string Title,
+        string[] Lines
+    )
+    {
+        public readonly int Width = Lines.Max(l => l.Length);
+
+        public string Line(int line)
+        {
+            return line < Lines.Length ? Lines[line] : "";
+        }
+    }
+
+    public static void PrintColumns(params Column[] columns)
+    {
+        var lines = columns.Max(column => column.Lines.Length);
+        const string delimiter = "  |  ";
+        var separator = new string('-', delimiter.Length * Math.Max(0, columns.Length - 1) + columns.Sum(c => c.Width));
+
+        Console.WriteLine(separator);
+        var title = string.Join(delimiter, columns.Select(c => string.Format($"{{0,-{c.Width}}}", c.Title)));
+        Console.WriteLine(title);
+        Console.WriteLine(separator);
+
+        for (var line = 0; line < lines; ++line)
+        {
+            var row = string.Join(delimiter, columns.Select(c => string.Format($"{{0,-{c.Width}}}", c.Line(line))));
+            Console.WriteLine(row);
+        }
+        Console.WriteLine(separator);
+    }
+
+    public static string PercentileDistributionToString(this LongHistogram histogram, double scalingFactor)
+    {
+        var writer = new StringWriter();
+        histogram.OutputPercentileDistribution(
+            writer,
+            outputValueUnitScalingRatio: scalingFactor,
+            percentileTicksPerHalfDistance: 1
+        );
+        return writer.ToString();
+    }
+
+    public static void PrintDictionaryIfNotEmpty(this Dictionary<string, uint> dictionary, string title)
+    {
+        if (0 < dictionary.Count)
+        {
+            Console.WriteLine($"{title}:");
+            var width = dictionary.Max(e => e.Key.Length);
+            foreach (var (message, count) in dictionary)
+            {
+                Console.WriteLine($"  {{0,-{width}}}  |  {count,6}  |", message);
+            }
+        }
+    }
+}
