1374c9932a233e07c090b5676b67d25ecd552cbd
Ruth Linehan <1530016+rlinehan@users.noreply.github.com>
2022-08-11 12:41:00 -0700

    chore: Expand example, add more comments and logs
    Update example to be more fleshed out:
    - create cache and signing key if they haven't already been created
    - add additional log statements and comments so example is more understable without additional context.
    - add to README for how to run this example

--- javascript/presigned-url-example.ts
+++ javascript/presigned-url-example.ts
@@ -1,34 +1,83 @@
-import {MomentoSigner, CacheOperation} from '@gomomento/sdk';
+import {
+  MomentoSigner,
+  CacheOperation,
+  SimpleCacheClient,
+  LogLevel,
+  LogFormat,
+  AlreadyExistsError,
+} from '@gomomento/sdk';
 import fetch, {Response} from 'node-fetch';
 
-const signingKey = requireEnvVar('MOMENTO_SIGNING_KEY');
-const signingEndpoint = requireEnvVar('MOMENTO_ENDPOINT');
-const cacheName = getCacheName();
+const cacheName = 'MyCache';
 const cacheKey = 'MyCacheKey';
 const cacheValue = 'MyCacheValue';
 const expiryEpochSeconds = Math.floor(Date.now() / 1000) + 10 * 60; // 10 minutes from now
 const objectTtlSeconds = 180;
 
+const authToken = requireEnvVar('MOMENTO_AUTH_TOKEN');
+const defaultTtl = 60;
+const momento = new SimpleCacheClient(authToken, defaultTtl, {
+  loggerOptions: {
+    level: LogLevel.DEBUG,
+    format: LogFormat.CONSOLE,
+  },
+});
+
 const main = async () => {
+  console.log(`Creating cache '${cacheName}'`);
+  try {
+    await momento.createCache(cacheName);
+  } catch (e) {
+    if (e instanceof AlreadyExistsError) {
+      console.log(`Cache '${cacheName}' already exists`);
+    } else {
+      throw e;
+    }
+  }
+
+  console.log(`Checking for signing key in $MOMENTO_SIGNING_KEY`);
+  let signingKey = process.env.MOMENTO_SIGNING_KEY;
+  if (!signingKey) {
+    // There is a limit of 5 signing keys per user, so it's best practice to
+    // create a signing key and store it somewhere secure. For convenience,
+    // this example creates a new signing key if one is not set in the
+    // environment variable `MOMENTO_SIGNING_KEY`. Signing keys can also be
+    // created and revoked via the Momento CLI.
+    console.log(
+      'Did not find signing key in environment, creating new signing key'
+    );
+    const signingKeyResponse = await momento.createSigningKey(60 * 24); // valid for 24 hours
+    signingKey = signingKeyResponse.getKey();
+    process.env.MOMENTO_ENDPOINT = signingKeyResponse.getEndpoint();
+  }
+  const endpoint = requireEnvVar('MOMENTO_ENDPOINT');
+
   const signer = await MomentoSigner.init(signingKey);
 
   console.log('\n*** Running presigned url example ***');
-  await runPresignedUrlExample(signer);
+  await runPresignedUrlExample(signer, endpoint);
 
   console.log('\n*** Running signed token example ***');
-  await runSignedTokenExample(signer);
+  await runSignedTokenExample(signer, endpoint);
 };
 
-async function runPresignedUrlExample(signer: MomentoSigner) {
-  // create presigned URL
-  const setUrl = await signer.createPresignedUrl(signingEndpoint, {
+// Create signed urls for setting and getting values via http for specified cache and
+// key
+async function runPresignedUrlExample(signer: MomentoSigner, endpoint: string) {
+  console.log(
+    `Creating signed url for ${CacheOperation.Set} for cache '${cacheName}' and key '${cacheKey}'`
+  );
+  const setUrl = await signer.createPresignedUrl(endpoint, {
     cacheName: cacheName,
     cacheKey: cacheKey,
     cacheOperation: CacheOperation.Set,
     ttlSeconds: objectTtlSeconds,
     expiryEpochSeconds: expiryEpochSeconds,
   });
-  const getUrl = await signer.createPresignedUrl(signingEndpoint, {
+  console.log(
+    `Creating signed url for ${CacheOperation.Get} for cache '${cacheName}' and key '${cacheKey}'`
+  );
+  const getUrl = await signer.createPresignedUrl(endpoint, {
     cacheName: cacheName,
     cacheKey: cacheKey,
     cacheOperation: CacheOperation.Get,
@@ -38,20 +87,29 @@ async function runPresignedUrlExample(signer: MomentoSigner) {
     `Created signed urls with claims: exp = ${expiryEpochSeconds}, cache = ${cacheName}, key = ${cacheKey}, ttl (for set) = ${objectTtlSeconds}`
   );
 
-  console.log(`Posting value with signed URL: '${cacheValue}'`);
+  console.log(
+    `Setting value for cache '${cacheName}' and key '${cacheKey}' via HTTP with signed URL: '${cacheValue}'`
+  );
   const setResponse = await fetch(setUrl, {method: 'POST', body: cacheValue});
   await ensureSuccessfulResponse(setResponse);
+  console.log(
+    `Getting value for cache '${cacheName}' and key '${cacheKey}' via HTTP with signed URL`
+  );
   const getResponse = await fetch(getUrl);
   await ensureSuccessfulResponse(getResponse);
 
   const data = await getResponse.text();
   console.log(`Retrieved value with signed URL: ${JSON.stringify(data)}`);
-  console.log('Presigned url example finished successfully!\n');
+  console.log('Presigned url example finished successfully!');
 }
 
-async function runSignedTokenExample(signer: MomentoSigner) {
-  // create signed token for this cache
-  const accessToken = await signer.signAccessToken({
+// Create signed token for specified cache. This token is then used for
+// setting and getting values via http. Signed tokens are restricted to a
+// specific cache, but unlike presigned urls they can be created without
+// restrictions on the operation and key they can be used for.
+async function runSignedTokenExample(signer: MomentoSigner, endpoint: string) {
+  console.log(`Creating new signed access token for cache '${cacheName}'`);
+  const signedToken = await signer.signAccessToken({
     cacheName: cacheName,
     expiryEpochSeconds: expiryEpochSeconds,
   });
@@ -61,18 +119,14 @@ async function runSignedTokenExample(signer: MomentoSigner) {
 
   // build urls
   const cacheKey = 'someKey';
-  const setUrl = buildSetUrl(
-    signingEndpoint,
-    cacheName,
-    cacheKey,
-    accessToken,
-    objectTtlSeconds
-  );
-  const getUrl = buildGetUrl(signingEndpoint, cacheName, cacheKey, accessToken);
+  const setUrl = `https://rest.${endpoint}/cache/set/${cacheName}/${cacheKey}?token=${signedToken}&ttl_milliseconds=${
+    objectTtlSeconds * 1000
+  }`;
+  const getUrl = `https://rest.${endpoint}/cache/get/${cacheName}/${cacheKey}?token=${signedToken}`;
 
   const cacheValue = 'somedata';
   console.log(
-    `Posting value via URL with signed token for key '${cacheKey}': '${cacheValue}'`
+    `Setting value for cache '${cacheName}' and key '${cacheKey}' via HTTP with signed token: '${cacheValue}'`
   );
   const setResponse = await fetch(setUrl, {method: 'POST', body: cacheValue});
   await ensureSuccessfulResponse(setResponse);
@@ -92,39 +146,6 @@ function requireEnvVar(envVarName: string): string {
     throw new Error(`Missing required environment variable ${envVarName}`);
   }
   return result;
-}
-
-function getCacheName(): string {
-  if (process.env.CACHE_NAME) {
-    return process.env.CACHE_NAME;
-  } else {
-    const defaultCacheName = 'default-cache';
-    console.log(
-      `Environment variable CACHE_NAME not set. Using cache name '${defaultCacheName}'.`
-    );
-    return defaultCacheName;
-  }
-}
-
-function buildSetUrl(
-  endpoint: string,
-  cacheName: string,
-  cacheKey: string,
-  token: string,
-  ttlSeconds: number
-): string {
-  return `https://rest.${endpoint}/cache/set/${cacheName}/${cacheKey}?token=${token}&ttl_milliseconds=${
-    ttlSeconds * 1000
-  }`;
-}
-
-function buildGetUrl(
-  endpoint: string,
-  cacheName: string,
-  cacheKey: string,
-  token: string
-): string {
-  return `https://rest.${endpoint}/cache/get/${cacheName}/${cacheKey}?token=${token}`;
 }
 
 async function ensureSuccessfulResponse(response: Response) {
