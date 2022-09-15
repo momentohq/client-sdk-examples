37ef802b4ef182cb6aa10953e488a06f08be08bf
erikatharp <eem@momentohq.com>
2022-09-15 15:46:16 -0700

    chore: remove java folder since Java example is moved to the client-skd-java repo

--- java/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Spotless plugin used for Java Formatting needs the buildscript with repository
-// to be defined in the ROOT_PROJECT
-// https://github.com/diffplug/spotless/issues/747
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-}
